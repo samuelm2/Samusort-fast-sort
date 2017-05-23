73db99ffa9820c07bad4b1d0959fdb2b354784ab
Samuel McFadden <samuelm2@illinois.edu>
2017-05-23 13:32:39 -0400

    Update Samusort.java
    Explained some things.

--- src/Samusort.java
+++ src/Samusort.java
@@ -2,7 +2,7 @@
  *
  * FASTER THAN ARRAY.SORT IN MOST CASES
  *
- * A sorting algorithm with a strange runtime that is based on the size of the array,
+ * A sorting algorithm with a runtime that is based on the size of the array,
  * the range of the array data, and the distribution of the data.
  *
  * The increased speed comes at the cost of increased memory usage, however.
@@ -11,6 +11,8 @@
  *
  * @author samuelmcfadden
  *
+ * Big O notation is in terms of the number of values in the array (n) and the distribution of the array (d)
+ * Runtime: O(n + d)
  */
 
 
@@ -18,18 +20,23 @@ public class Samusort {
 
     public static void samusort(int[] array) {
 
-        int min = findMin(array);
-        int max = findMax(array);
+        int min = findMin(array); //O(n)
+        int max = findMax(array); //O(n)
 
-        int[] possibleValCount = new int[max-min + 1];
+        int[] possibleValCount = new int[max-min + 1]; //O(d)
 
-        for(int i = 0; i < array.length; i++) {
+        //Counts the number of each value in the array
+        for(int i = 0; i < array.length; i++) { //O(n)
             possibleValCount[array[i] - min] ++;
         }
 
         int arrayIndex = 0;
 
-        for(int i = 0; i < possibleValCount.length; i++) {
+        for(int i = 0; i < possibleValCount.length; i++) { //O(d)
+            /*
+              This for loop will iterate a total of n times.
+              It assigns the value of i + min (which is a number that was originally in the array) to its correct position.
+            */
             for(int j = possibleValCount[i]; j> 0; j--) {
                 array[arrayIndex] = (i + min);
                 arrayIndex ++;
